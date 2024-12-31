e04765caf9a78e95f46a1452b44ea971db3d6c62
AKRITI <akritigupta022@gmail.com>
2024-12-31 23:50:24 +0530

    Initial commit with index.html and dom.js

---
+++ dom.js
@@ -0,0 +1,26 @@
+//? HERE AM GONNA CREATE THE HTML elements INSIDE THE JS BY USING DOM also this is what does 
+
+
+ const heading =document.createElement('h1'); //* here am creating the element inside my js file ie h1
+
+ heading.innerHTML="NAMASTE REACT"; //-here am creating the innerHTML inside my h1 element 
+
+ 
+
+ 
+
+document.querySelector('#root').appendChild(heading);
+
+
+
+document.body.style.background="pink"
+
+
+
+/**
+ * //-  my first comment
+ * //*  my second comment
+ * //Todo  my third comment
+ * //! my forth comment
+ * //? my fifth comment
+ */
